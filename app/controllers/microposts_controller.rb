9f02537d1d0f557aae0232b14ae2e08052822bf4
Gustavo Del Castillo <agamenong@gmail.com>
2011-03-21 22:43:13 -0700

    Finished microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 
   before_filter :authenticate
+  before_filter :authorize_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -8,13 +9,22 @@ class MicropostsController < ApplicationController
     if @micropost.save
       redirect_to root_path, :flash => { :success => "Micropost created!" }
     else
+      @feed_items = []
       render 'pages/home'
     end    
         
   end
 
   def destroy
-    
+    @micropost.destroy
+    redirect_to root_path, :flash => { :success => "Micropost deleted!" }
   end
+  
+  private 
+    
+    def authorize_user
+      @micropost = Micropost.find(params[:id])
+      redirect_to root_path unless current_user?(@micropost.user)
+    end
   
 end
